7ec4e353304c52c25fe5f630bf4b14c874e4cbfa
Vinicius <vi058590@intelbras.com.br>
2023-11-27 21:14:09 -0300

    chore: change to my data

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 const conta = {
-    email: 'nath@dio.bank',
-    password: '123456',
-    name: 'Nathaly Souza',
+    email: 'vini@dio.bank',
+    password: '123456789',
+    name: 'Vinicius de Lima',
     balance: 2000.00,
     id: '1'
 }
